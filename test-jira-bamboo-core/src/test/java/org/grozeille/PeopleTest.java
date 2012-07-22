577e55c34b5cb95762eb2e281775bced4b4985c0
Mathias Kluba <mathias.kluba@gmail.com>
2012-07-22 10:37:22 +0200

    Add test for fixing TST-7

--- test-jira-bamboo-core/src/test/java/org/grozeille/PeopleTest.java
+++ test-jira-bamboo-core/src/test/java/org/grozeille/PeopleTest.java
@@ -6,5 +6,16 @@ import org.junit.Test;
 
 public class PeopleTest {
   
-  
+  @Test
+  public void test(){
+    People mathias = new People();
+    mathias.setFirstname("Mathias");
+    mathias.setLastname("Kluba");
+    
+    People michel = new People();
+    michel.setFirstname("Michel");
+    michel.setLastname("Kluba");
+    
+    Assert.assertEquals("Hello Mathias!", michel.SayHelloTo(mathias));
+  }
 }
